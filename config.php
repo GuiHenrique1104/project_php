a19e53d5a2bfc6ceaf4be8bf7e73d62a06a9d6bb
guilherme henrique <gui.oliveira1104@outlook.com>
2022-08-25 21:15:33 -0300

    finalizando projeto

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
     define('HOST', 'localhost');
     define('USER', 'guilherme');
-    define('PASS', 'gui241113');
+    define('PASS', '9Y9l_RB9[W8ekKKY');
     define('BASE', 'crud_php');
 
     $conn = new MySQLi(HOST, USER, PASS, BASE);
